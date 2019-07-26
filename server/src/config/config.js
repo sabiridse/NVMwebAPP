bc6166b471ab9c21c58c95ef4b505e527029416b
AbstractMac <abstractmac@MacBook-Air-AbstractMac.local>
2019-07-26 12:29:00 +0300

    serverside and mongoDB migrated on remote host

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,5 +1,5 @@
 module.exports={
 	port: 8082,
-	dbURL: 'mongodb://localhost/budgetDB',
-  	dbOptions: { useNewUrlParser: true }//ljfjlsfkl
+	dbURL: 'mongodb://ribasBudgetDB:8$imvoloV@localhost:27017/budgetDB',
+	dbOptions: { useNewUrlParser: true }//ljfjlsfkl
 }
